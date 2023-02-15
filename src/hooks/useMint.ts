c53d67c61e66d822524f69b68bebf0566e8123b3
Carl Barrdahl <carlbarrdahl@gmail.com>
2023-02-15 07:26:59 +0100

    Update hook to use hypercerts sdk

--- src/hooks/useMint.ts
+++ src/hooks/useMint.ts
@@ -1,16 +1,48 @@
-import { useContractWrite } from "wagmi";
+import { useContractWrite, useMutation } from "wagmi";
 import { SendTransactionResult } from "@wagmi/core";
 
+import {
+  HypercertMetadata,
+  storeMetadata,
+  validateMetaData,
+} from "@hypercerts-org/hypercerts-sdk";
+import {
+  mintHypercertToken,
+  transferRestrictions,
+} from "@hypercerts-org/hypercerts-sdk/lib/minting";
+
+import { ipfsClient } from "utils/ipfs";
 import { useContractConfig } from "./useContractConfig";
 
-export const useMint = (onSuccess: (data: SendTransactionResult) => void) => {
+export const useMintHypercert = (
+  onSuccess: (data: SendTransactionResult) => void
+) => {
   const { abi, address } = useContractConfig("HypercertMinter");
 
-  return useContractWrite({
+  const mint = useContractWrite({
     address: address as `0x${string}`,
     abi,
     functionName: "mintClaim",
     mode: "recklesslyUnprepared",
     onSuccess,
+  });
+
+  return useMutation(async (claimData: HypercertMetadata) => {
+    if (validateMetaData(claimData)) {
+      return storeMetadata(claimData, ipfsClient).then((cid) => {
+        console.log("Metadata stored:", cid);
+
+        const args = [1, cid, transferRestrictions.AllowAll];
+        console.log("Minting Hypercert:", args);
+
+        return mint.write?.({ recklesslySetUnpreparedArgs: args });
+
+        // TODO: Doesn't work because NFT.Storage token is currently not possible to set
+        // return mintHypercertToken(claimData, 1, transferRestrictions.AllowAll);
+      });
+    } else {
+      console.log("Incorrect metadata");
+      return null;
+    }
   });
 };

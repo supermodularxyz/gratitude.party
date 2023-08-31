99e05e16aadbf5f89f4c22025d232189c5a4e161
memosys <82053242+memosys@users.noreply.github.com>
2023-08-31 09:50:15 +0530

    Chakra Form update
    Todo: full functionality

--- src/components/GreenPillForm.tsx
+++ src/components/GreenPillForm.tsx
@@ -1,106 +1,101 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { useFormContext } from "react-hook-form";
-import React from "react";
-import { DateRangeInput, Textarea } from "components/Form";
-import { useEnsName } from 'wagmi';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import React from 'react';
+import * as z from 'zod';
+import { SingleDatepicker } from "chakra-dayzed-datepicker";
+import { Box, FormControl, FormLabel, Input, Button, Textarea } from '@chakra-ui/react';
 
-export const content = {
-  intro: "I would like to give thanks to",
-};
+const schema = z.object({
+  name: z.string().min(1, { message: 'Required' }),
+  tags: z.string().min(1, { message: 'Required' }),
+  link: z.string().url({ message: 'Invalid URL' }),
+  description: z.string().min(1, { message: 'Required' }),
+  others: z.string().optional(),
+  startDate: z.date(),
+  endDate: z.date(),
+});
 
-// chakra migration
-
-import {
-    // FormErrorMessage,
-    FormLabel,
-    FormControl,
-    Input
-  } from '@chakra-ui/react'
-
-// export const gratitudeTemplate = ({ contributor = "", Description = "" }) =>
-//   `${content.intro} ${contributor} for ${Description}`;
-
-const MAX_DESCRIPTION_LENGTH = 144;
-
-export const GreenPillForm = () => {
-  const { watch, setError, setValue } = useFormContext();
-
-  const contributor = watch("contributor");
-  const Description:string = watch("Description") || "";
-
-    const {data: ens} = useEnsName({address: contributor})
-
-
-
-
+const GreenPillForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
 
   return (
+    <form onSubmit={handleSubmit((d) => console.log(d))}>
+      <Box fontSize="lg" color="green.900">
+        <FormControl id="name">
+          <FormLabel>Name of your Chapter</FormLabel>
+          <Input
+            {...register('name')}
+            isInvalid={errors.name ? true : false}
+            autoFocus
+            mb={4}
+            w="full"
+            placeholder=""
+          />
+        </FormControl>
+        <FormControl id="tags">
+          <FormLabel>Tags for Scope of Work</FormLabel>
+          <Input
+            {...register('tags')}
+            isInvalid={errors.tags ? true : false}
+            placeholder="Social Impact, Public Health, Education, etc."
+          />
+        </FormControl>
+        <FormControl id="link">
+          <FormLabel>Link where we can find info about work</FormLabel>
+          <Input
+            {...register('link')}
+            isInvalid={errors.link ? true : false}
+            autoFocus
+            mb={4}
+            w="full"
+            placeholder=""
+          />
+        </FormControl>
+        <FormControl id="description">
+          <FormLabel>Description of the Work</FormLabel>
+          <Textarea
+            {...register('description')}
+            isInvalid={errors.description ? true : false}
+            rows={6}
+            placeholder="Social Impact, Public Health, Education, etc."
+          />
+        </FormControl>
+        <FormControl id="others">
+          <FormLabel>Any Other Contributors?</FormLabel>
+          <Textarea
+            {...register('others')}
+            isInvalid={errors.others ? true : false}
+            rows={6}
+            placeholder="You can add names, address of contributors that consent to be registered publicly."
+          />
+        </FormControl>
 
-    <div className="text-lg text-green-900 gap-4">
-      <div className="flex flex-row justify-between w-full gap-2">
-        <div className="flex flex-col w-full">
-            
-      <FormControl label={'Name of your Chapter'}>
-        <FormLabel htmlFor="name">Name of your Chapter</FormLabel>
-        <Input
-          required
-          autoFocus
-          className="mb-4 w-full"
-          placeholder=""
-        />
-      </FormControl>
-      <FormControl label={"Tags for Scope of Work"}>
-      <FormLabel htmlFor="tags">Tags for Scope of Workr</FormLabel>
-        <Input
-          maxLength={MAX_DESCRIPTION_LENGTH}
-          required
-          placeholder="Social Impact, Public Health, Education, etc."
-        />
-      </FormControl>
-      <FormControl label={"Duration of Contibution"} className="my-4">
-      <FormLabel htmlFor="duration">Duration of Contibution</FormLabel>
-        <DateRangeInput 
-          nameStart="start"
-          nameEnd="end"
-          labelStart="Start Date"
-          labelEnd="End Date"
-          className="mb-4 w-full"
-        />
-      </FormControl>
-      <FormControl label={"Link where we can find info about work"}>
-      <FormLabel htmlFor="link">Link where we can find info about work</FormLabel>
-      <Input
-          required
-          autoFocus
-          className="mb-4 w-full"
-          placeholder=""
-        />
-      </FormControl>
-      </div>
-      <div className="bg-green-200 border-green-600 border-1  items-center w-96">
-        {/* TODO: PREVIEW IMAGE HERE */}
-        Preview Image
-      </div>
-      </div>
-      <FormControl label={"Description of the work delivered work"}>
-      <FormLabel htmlFor="description">Description of the work delivered work</FormLabel>
-        <Textarea
-          maxLength={MAX_DESCRIPTION_LENGTH}
-          rows={6}
-          required
-          placeholder="Social Impact, Public Health, Education, etc."
-        />
-      </FormControl>
-      <FormControl label={"Any Other Contributors?"}>
-      <FormLabel htmlFor="others">Any Other Contributors?</FormLabel>
-        <Textarea
-          maxLength={MAX_DESCRIPTION_LENGTH}
-          rows={6}
-          required
-          placeholder="You can add names, address of contributors that consent to be registered publicly."
-        />
-      </FormControl>
-      
-    </div>
+        <FormControl id="startDate">
+          <FormLabel>Start Date</FormLabel>
+          <SingleDatepicker
+          {...register('startDate')}
+          onDateChange={(date) => console.log(date)}
+          />
+        </FormControl>
+
+        <FormControl id="endDate">
+          <FormLabel>End Date</FormLabel>
+          <SingleDatepicker
+          {...register('endDate')}
+          onDateChange={(date) => console.log(date)}
+          />
+        </FormControl>
+
+        <Button type="submit">Submit</Button>
+      </Box>
+    </form>
   );
 };
+
+export default GreenPillForm;
--- src/pages/design.tsx
+++ src/pages/design.tsx
@@ -1,19 +1,13 @@
-import { type NextPage } from "next";
+import { Button, Text, Box, Flex, Heading } from "@chakra-ui/react";
+import type { NextPage } from "next";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import z from "zod";
-// import { useFormContext } from "react-hook-form";
 
+import GreenPillForm from "components/GreenPillForm";
+import { useRouter } from "next/router";
+import * as z from "zod";
 import { Layout } from "../layouts/Layout";
-// import { Button } from "../components/Button";
-// import { Designer } from "components/Designer";
-import { Form } from "../components/Form";
-import { GreenPillForm } from "../components/GreenPillForm";
-import { generateSVG } from "../utils/svg";
 import { createClaim } from "../utils/createClaim";
-import React from "react";
-//chakra migration
-import {Button} from '@chakra-ui/react'
+import { generateSVG } from "../utils/svg";
 
 const Schema = z.object({
   contributor: z.string(),
@@ -21,49 +15,36 @@ const Schema = z.object({
   reason: z.string(),
 });
 
-const CurrentStep = ({ step = "text", isMinting = false }) => {
-  // const form = useFormContext();
+type QueryParams = {
+  step: "text" | "design";
+};
 
+type CurrentStepProps = {
+  step?: "text" | "design";
+  isMinting?: boolean;
+};
+
+const CurrentStep: React.FC<CurrentStepProps> = ({
+  step = "text",
+  isMinting = false,
+}) => {
   switch (step) {
     case "text":
-      return (
-        <>
-          <GreenPillForm />
-          <div className="flex justify-center">
-            <Button
-              className="w-48"
-              as={Link}
-              // disabled={!form.formState.isValid}
-              href={"?step=design"}
-              color="gradient"
-            >
-              Next
-            </Button>
-          </div>
-        </>
-      );
+      return <GreenPillForm />;
     case "design":
       return (
-        <>
-          {/* <Designer {...form.watch()} /> */}
-          <div className="flex justify-between">
-            <Button as={Link} href={"?step=text"} color="ghost">
-              Back
-            </Button>
-            <Button
-              className="w-48"
-              disabled={isMinting}
-              type="submit"
-              color="gradient"
-            >
-              {isMinting ? "Generating..." : "Generate"}
-            </Button>
-          </div>
-        </>
+        <Flex justifyContent="space-between">
+          <Button as={Link} href={"?step=text"} colorScheme="teal">
+            Back
+          </Button>
+          <Button w="48" isLoading={isMinting} type="submit" colorScheme="blue">
+            Generate
+          </Button>
+        </Flex>
       );
+    default:
+      return <Text>Something Went Wrong</Text>;
   }
-
-  return null;
 };
 
 const headings = {
@@ -73,44 +54,44 @@ const headings = {
 
 const Design: NextPage = () => {
   const router = useRouter();
-  // const design = useDesign();
   const step = router.query.step as string;
 
-  // const mint = useMint((data) => router.push(`/tx/${data.hash}`));
   return (
     <Layout>
-      <Form
-        schema={Schema as never}
-        onSubmit={async ({ contributor, contributorAddress, reason }) => {
-          // const description = gratitudeTemplate({ contributor, reason });
+      <form
+        onSubmit={async (event: React.FormEvent<HTMLFormElement>) => {
+          event.preventDefault();
+          const formData = new FormData(event.currentTarget);
+          const contributor = formData.get('contributor') as string;
+          const contributorAddress = formData.get('contributorAddress') as string;
+          const reason = formData.get('reason') as string;
+
           const svg = await generateSVG({
             contributor,
             reason,
-            // ...design,
           });
 
-          // eslint-disable-next-line @typescript-eslint/no-unused-vars
           const claimData = createClaim({
-            // description,
             svg,
             contributor,
             contributorAddress,
           });
 
-          // mint.mutate({ contributor: contributorAddress, claimData });
+          console.log(claimData);
         }}
       >
-        <h1 className="mb-6 text-center text-4xl font-bold text-green-900">
-          {headings[step as keyof typeof headings]}
-        </h1>
-        <CurrentStep
-          step={router.query.step as string}
-          // isMinting={mint.isLoading}
-        />
-      </Form>
-      <div className="pt-4 font-mono text-sm text-red-600">
-        {/* {(mint.error as any)?.message} */}
-      </div>
+        <Heading
+          fontSize="4xl"
+          fontFamily="bold"
+          color="green.900"
+          mb={6}
+          textAlign="center"
+        >
+          {headings[step as never]}
+        </Heading>
+        <CurrentStep step={step as never} />
+      </form>
+      <Box pt={4} fontFamily="monospace" fontSize="sm" color="red.600"></Box>
     </Layout>
   );
 };
